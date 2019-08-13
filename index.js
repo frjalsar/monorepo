a76ae047c3819880694b8941d5af923f3ca34d33
Bergur Hallgrimsson <bergur@bergur.is>
2019-08-13 17:19:31 +0000

    adding helmet and enforce https

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ require('dotenv').config()
 const express = require('express')
 const compression = require('compression')
 const bodyParser = require('body-parser')
-const cookieParser = require('cookie-parser')
+const helmet = require('helmet')
+const enforceHttps = require('express-sslify').HTTPS
 const cors = require('cors')
+
 const authorize = require('./auth/authorize')
 
 // Routes
@@ -13,11 +15,17 @@ const clubRoute = require('./clubs/route')
 const regionRoute = require('./regions/route')
 const passesRoute = require('./passes/route')
 const loginRoute = require('./auth/route')
-
 const app = express()
-app.use(compression())
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(enforceHttps())
+  app.use(compression())
+  app.use(helmet())
+}
+
 const whitelist = [
   'http://local.fri.is:1234',
+  'http://local.fri.is:3010',
   'https://felagatal.fri.is'
 ]
 app.use(cors({
@@ -29,8 +37,8 @@ app.use(cors({
     }
   },
   credentials: true
-})) // All sites, fix before launch
-app.use(cookieParser())
+}))
+
 app.use(bodyParser.json({ limit: '40mb' }))
 app.use(bodyParser.urlencoded({ extended: false, limit: '50mb' }))
 
