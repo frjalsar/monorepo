935e1e6d5a5379206f12c13188f75fc4ea6870e9
Bergur Hallgrímsson <bergur@bergur.is>
2019-12-08 19:43:36 +0000

    API for Thor Achievements

--- routes/thor.js
+++ routes/thor.js
@@ -1,13 +1,20 @@
 const express = require('express')
 const makeSelectThorEvents = require('../lib/thor/events')
+const makeSelectThorAchievements = require('../lib/thor/achievements')
 
 function makeThorRoute (sqlConnection) {
   const router = express.Router()
+  const selectThorEvents = makeSelectThorEvents(sqlConnection)
+  const selectThorAchievements = makeSelectThorAchievements(sqlConnection)
 
   router.get('/events', (req, res, next) => {
-    const selectThorEvents = makeSelectThorEvents(sqlConnection)
-
     selectThorEvents(req.query)
+      .then(res.json.bind(res))
+      .catch(next)
+  })
+
+  router.get('/achievements', (req, res, next) => {
+    selectThorAchievements(req.query)
       .then(res.json.bind(res))
       .catch(next)
   })
