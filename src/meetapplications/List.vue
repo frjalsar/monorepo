3880f60aebf6bce8356bcd3e107072ad31f73fa3
bergur <bergur@bergur.is>
2022-01-18 00:41:22 +0000

    add empty components

---
+++ src/meetapplications/List.vue
@@ -0,0 +1,17 @@
+<template>
+<div>
+  <h2>Mótaumsóknir</h2>
+  <hr />
+  <div class="card shadow-sm mb-3">
+    <div class="card-body">      
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'MeetApplicationsList',
+  inject: ['FRI_API_URL']
+}
+</script>
---
+++ src/venues/List.vue
@@ -0,0 +1,17 @@
+<template>
+<div>
+  <h2>Mannvirki</h2>
+  <hr />
+  <div class="card shadow-sm mb-3">
+    <div class="card-body">      
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'VenuesList',
+  inject: ['FRI_API_URL']
+}
+</script>
