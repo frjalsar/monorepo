b76b75261038ccb6f94780262c1cce570b7ac944
bergur <bergur@bergur.is>
2022-01-19 17:45:47 +0000

    add confirm on membership

--- src/athletes/Edit.vue
+++ src/athletes/Edit.vue
@@ -2,18 +2,18 @@
 <form>
   <div class="row g-3 mb-3">
     <div class="col-md-6">
-      <label for="name" class="form-label">Nafn</label>
-      <input type="name" class="form-control" id="name" v-model="currentItem.fullName">
+      <label for="fullName" class="form-label">Nafn</label>
+      <input type="text" class="form-control" id="fullName" v-model="currentItem.fullName">
     </div>
 
     <div class="col-md-4">
-      <label for="name" class="form-label">Kennitala</label>
-      <input type="name" class="form-control" id="name" v-model="currentItem.kt">
+      <label for="kt" class="form-label">Kennitala</label>
+      <input type="text" class="form-control" id="kt" v-model="currentItem.kt">
     </div>
 
     <div class="col-md-2">
-      <label for="name" class="form-label">Fæðingarár</label>
-      <input type="name" class="form-control" id="name" v-model="currentItem.birthyear">
+      <label for="birthyear" class="form-label">Fæðingarár</label>
+      <input type="tel" class="form-control" id="birthyear" v-model="currentItem.birthyear">
     </div>
   </div>
 
@@ -38,25 +38,25 @@
   <div
     class="row g-3 mb-3"
     v-for="(m, index) in currentItem.membership"
-    :key="m.clubId + '-' + m.from"
+    :key="m.clubId + '-' + m.athleteId"
   >
     <div class="col-md-6">
-      <label for="name" class="form-label" v-if="index === 0">Félag</label>      
-      <select class="form-select" v-model="m.clubId">
+      <label :for="'club- '+ index" class="form-label" v-if="index === 0">Félag</label>      
+      <select class="form-select" v-model="m.clubId" :id="'club-' + index">
         <option v-for="club in clubsByRegion(m.legacyClub)" :key="club.id" :value="club.id">{{ club.fullName }}</option>        
       </select>
       <div v-if="m.legacyClub" class="form-text">Viðkomandi var skráður í <em>{{ m.legacyClub }}</em> en ekki félag í gamla grunninum. Vinsamlegast lagið.</div>
     </div>
     
     <div class="col-md-2">
-      <label for="name" class="form-label" v-if="index === 0">Frá</label>
-      <input type="name" class="form-control" id="name" v-model="m.from">
+      <label :for="'from-' + index" class="form-label" v-if="index === 0">Frá</label>
+      <input type="tel" class="form-control" :for="'from-' + index" v-model="m.from">
     </div>
 
     <div class="col-md-2">
-      <label for="name" class="form-label" v-if="index === 0">Til</label>
-      <input type="name" class="form-control" id="name" v-model="m.to">
-    </div>
+      <label :for="'from-' + index" class="form-label" v-if="index === 0">Til</label>
+      <input type="tel" class="form-control"  :for="'to-' + index" v-model="m.to">
+    </div>    
 
     <div class="col-md-1 offset-md-1">
       <div class="form-label" v-if="index === 0">Aðgerð</div>
@@ -74,6 +74,17 @@
     </div>
   </div>
 
+  <div class="row g-3 mb-3">
+    <div class="col-md-12">
+      <div class="form-check">
+        <input class="form-check-input" type="checkbox" v-model="membershipIsConfirmed" @change="confirmAllMembership()" id="felagasaga">
+        <label class="form-check-label" for="felagasaga">
+          Staðfesta skráningu í félagasögu.
+        </label>
+</div>
+    </div>
+  </div>
+
 </form>
 </template>
 
@@ -87,6 +98,7 @@ export default {
     return {
       busy: false,
       currentItem: this.athlete,
+      membershipIsConfirmed: false,
       genders: [{
         value: 1,
         text: 'Karl'
@@ -112,7 +124,14 @@ export default {
       }
       
       this.currentItem.membership.push({})
-    }
+    },
+    confirmAllMembership () {
+      this.currentItem.membership.forEach(m => {
+        m.confirmed = this.membershipIsConfirmed
+      })
+
+      console.log(this.currentItem.membership)
+    },
   },
   watch: {
     athlete (val) {
