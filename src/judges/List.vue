6f80081d94d5600fa116232a07c347ccce47b93e
bergur <bergur@bergur.is>
2022-01-21 23:30:22 +0000

    judges

--- src/judges/List.vue
+++ src/judges/List.vue
@@ -1,17 +1,91 @@
 <template>
 <div>
-  <h2>Dómarar</h2>
-  <hr />
-  <div class="card shadow-sm mb-3">
-    <div class="card-body">      
-    </div>
-  </div>
+  <Title text="Dómarar" @addNewItem="openEditModal({})" />
+
+  <Card>
+    <SimpleTable
+      :data="judges"
+      :definition="tableDefinition"
+      @click="openEditModal"
+    />
+  </Card>
+
+  <ModalEdit v-slot="{ confirm, callback }"> 
+    <EditClub
+      :judge="selectedModalItem"
+      :clubs="clubs"
+      :confirm="confirm"
+      @done="closeEditModal(callback)"
+    />
+  </ModalEdit>
 </div>
 </template>
 
 <script>
+import agent from 'superagent'
+import Title from '../_components/Title.vue'
+import Card from '../_components/Card.vue'
+import SimpleTable from '../_components/SimpleTable.vue'
+import ModalEdit from '../_components/EditModal.vue'
+import ModalEditMixin from '../_mixins/ModalMixin.vue'
+import EditClub from './Edit.vue'
+
 export default {
-  name: 'JudgesList',
-  inject: ['FRI_API_URL']
+  name: 'ListJudges',
+  mixins: [ModalEditMixin],
+  components: {
+    Title,
+    Card,
+    SimpleTable,
+    ModalEdit,
+    EditClub
+  },
+  inject: ['FRI_API_URL'],
+  data() {
+    return {
+      judges: [],
+      clubs: [],      
+      tableDefinition: [
+        {
+          field: 'id',
+          label: '#',
+          display: 'lg'
+        },
+        {
+          field: 'fullName',
+          label: 'Nafn',
+          display: 'xs'
+        },
+        {
+          field: 'date',
+          label: 'Dags. réttinda',
+          display: 'md'
+        },              
+        {
+          field: 'clubFullName',
+          label: 'Félag',
+          display: 'md'
+        },
+      ]
+    }
+  },
+  created() {
+    agent
+      .get(this.FRI_API_URL + '/judges')
+      .withCredentials()
+      .then(res => {
+        this.judges = res.body.map(item => ({
+          ...item,
+          clubFullName: item.club?.fullName
+        }))
+      })
+
+    agent
+      .get(this.FRI_API_URL + '/clubs')
+      .withCredentials()
+      .then(res => {
+        this.clubs = res.body
+      })
+  }
 }
 </script>
