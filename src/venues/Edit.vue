22e5271da8f3140ef93bf810aef63cac62b23784
bergur <bergur@bergur.is>
2022-01-22 03:00:44 +0000

    mannvirki

---
+++ src/venues/Edit.vue
@@ -0,0 +1,146 @@
+<template>
+<form>
+  <div class="row g-3 mb-3">
+    <div class="col-md-6">
+      <label for="fullName" class="form-label">Fullt nafn</label>
+      <input id="fullName" type="text" class="form-control"  v-model="currentItem.fullName" :disabled="busy">
+    </div>
+
+    <div class="col-md-2">
+      <label for="inddor" class="form-label">Innahús</label>      
+      <select id="inddor" class="form-select" v-model="currentItem.indoor" :disabled="busy">
+        <option v-for="opt in yesNo" :key="opt.value" :value="opt.value">{{ opt.text }}</option>        
+      </select>
+    </div>
+
+     <div class="col-md-2">
+      <label for="banked" class="form-label">Hallandi</label>      
+      <select id="banked" class="form-select" v-model="currentItem.banked" :disabled="busy">
+        <option v-for="opt in yesNo" :key="opt.value" :value="opt.value">{{ opt.text }}</option>        
+      </select>
+    </div>
+
+    <div class="col-md-2">
+      <label for="straightLanes" class="form-label">Beinar brautir</label>
+      <input id="straightLanes" type="tel" class="form-control" v-model="currentItem.straightLanes" :disabled="busy">
+    </div>
+  </div>
+
+  <div class="row g-3 mb-3">
+    <div class="col-md-2">
+      <label for="ovalLanes" class="form-label">Hringbrautir</label>
+      <input id="ovalLanes" type="tel" class="form-control" v-model="currentItem.ovalLanes" :disabled="busy">
+    </div>
+
+    <div class="col-md-2">
+      <label for="longTripleJump" class="form-label">Lang/þrístökk</label>
+      <input id="longTripleJump" type="tel" class="form-control" v-model="currentItem.longTripleJump" :disabled="busy">
+    </div>
+
+    <div class="col-md-2">
+      <label for="polevault" class="form-label">Stangarstökk</label>
+      <input id="polevault" type="tel" class="form-control" v-model="currentItem.polevault" :disabled="busy">
+    </div>
+
+    <div class="col-md-2">
+      <label for="hammerDiscus" class="form-label">Sleggja/kringla</label>
+      <input id="hammerDiscus" type="tel" class="form-control"  v-model="currentItem.hammerDiscus" :disabled="busy">
+    </div>
+
+    <div class="col-md-2">
+      <label for="javelin" class="form-label">Spjótkast</label>
+      <input id="javelin" type="tel" class="form-control" v-model="currentItem.javelin" :disabled="busy">
+    </div>
+
+    <div class="col-md-2">
+      <label for="shotPut" class="form-label">Kúluvarp</label>
+      <input id="shotPut" type="tel" class="form-control" v-model="currentItem.shotPut" :disabled="busy">
+    </div>
+  </div>
+
+  <div class="row g-3 mb-3">
+    <div class="col-md-4">
+      <label for="material" class="form-label">Efni</label>
+      <input id="material" type="text" class="form-control"  v-model="currentItem.material" :disabled="busy">
+    </div>
+
+    <div class="col-md-2">
+      <label for="certification" class="form-label">IAAF staðfest</label>      
+      <select id="certification" class="form-select" v-model="currentItem.certification" :disabled="busy">
+        <option v-for="opt in yesNo" :key="opt.value" :value="opt.value">{{ opt.text }}</option>        
+      </select>
+    </div>
+
+    <div class="col-md-2">
+      <label for="constructionYear" class="form-label">Byggingarár</label>
+      <input id="constructionYear" type="tel" class="form-control" v-model="currentItem.constructionYear" :disabled="busy">
+    </div>
+
+    <div class="col-md-2">
+      <label for="renovationYear" class="form-label">Endurnýjað (ár)</label>
+      <input id="renovationYear" type="tel" class="form-control" v-model="currentItem.renovationYear" :disabled="busy">
+    </div>
+
+    <div class="col-md-2">
+      <label for="warmupTrack" class="form-label">Upphitunarvöllur</label>      
+      <select id="warmupTrack" class="form-select" v-model="currentItem.warmupTrack" :disabled="busy">
+        <option v-for="opt in yesNo" :key="opt.value" :value="opt.value">{{ opt.text }}</option>        
+      </select>
+    </div>
+  </div>
+
+  <div class="row g-3 mb-3">
+    <div class="col-md-2">
+      <label for="throwingField" class="form-label">Kastvöllur</label>      
+      <select id="throwingField" class="form-select" v-model="currentItem.throwingField" :disabled="busy">
+        <option v-for="opt in yesNo" :key="opt.value" :value="opt.value">{{ opt.text }}</option>        
+      </select>
+    </div>
+
+    <div class="col-md-1">
+      <label for="id" class="form-label">Númer</label>
+      <input id="id" type="name" class="form-control-plaintext" readonly v-model="currentItem.id" :disabled="busy">
+    </div>
+  </div>  
+
+</form>
+</template>
+
+<script>
+import EditMixin from '../_mixins/EditMixin.vue'
+export default {
+  name: 'EditVenues',
+  inject: ['FRI_API_URL'],
+  mixins: [EditMixin],
+  props: ['venue'],
+  data() {
+    return {
+      yesNo: [
+        {
+          value: true,
+          text: 'Já'
+        }, {
+          value: false,
+          text: 'Nei'
+        }
+      ],
+    }
+  },
+  computed: {
+    apiUrl() {
+      return this.FRI_API_URL + '/venues'
+    }
+  },  
+  watch: {
+    venue (val) {
+      if (val && val.id) {
+        this.currentItem = val
+      } else {
+        this.currentItem = {
+          region: {}
+        }
+      }
+    },    
+  },  
+}
+</script>
--- src/venues/List.vue
+++ src/venues/List.vue
@@ -1,17 +1,87 @@
 <template>
 <div>
-  <h2>Mannvirki</h2>
-  <hr />
-  <div class="card shadow-sm mb-3">
-    <div class="card-body">      
-    </div>
-  </div>
+  <Title text="Mannvirki" @addNewItem="openEditModal({})" />
+
+  <Card>
+    <SimpleTable
+      :data="venues"
+      :definition="tableDefinition"
+      @click="openEditModal"
+    />
+  </Card>
+
+  <ModalEdit v-slot="{ confirm, callback }"> 
+    <EditClub
+      :venue="selectedModalItem"
+      :confirm="confirm"
+      @done="(isDone) => closeEditModal(isDone, callback)"
+    />
+  </ModalEdit>
 </div>
 </template>
 
 <script>
+import agent from 'superagent'
+import Title from '../_components/Title.vue'
+import Card from '../_components/Card.vue'
+import SimpleTable from '../_components/SimpleTable.vue'
+import ModalEdit from '../_components/EditModal.vue'
+import ModalEditMixin from '../_mixins/ModalMixin.vue'
+import EditClub from './Edit.vue'
+
 export default {
-  name: 'VenuesList',
-  inject: ['FRI_API_URL']
+  name: 'ClubList',
+  mixins: [ModalEditMixin],
+  components: {
+    Title,
+    Card,
+    SimpleTable,
+    ModalEdit,
+    EditClub
+  },
+  inject: ['FRI_API_URL'],
+  data() {
+    return {
+      venues: [],
+      tableDefinition: [
+        {
+          field: 'id',
+          label: '#',
+          display: 'lg'
+        },
+        {
+          field: 'fullName',
+          label: 'Nafn',
+          display: 'xs'
+        },
+        {
+          field: 'indoorText',
+          label: 'Innanhús',
+          display: 'lg'
+        },       
+        {
+          field: 'straightLanes',
+          label: 'Beinar brauta',
+          display: 'md'
+        },
+         {
+          field: 'ovalLanes',
+          label: 'Hringbrautir',
+          display: 'md'
+        },
+      ]
+    }
+  },
+  created() {
+    agent
+      .get(this.FRI_API_URL + '/venues')
+      .withCredentials()
+      .then(res => {
+        this.venues = res.body.map(item => ({
+          ...item,
+          indoorText: item.indoor ? 'Já' : 'Nei'
+        }))
+      })
+  }
 }
 </script>
