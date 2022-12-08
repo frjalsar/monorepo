18adac89f227f560430fe8e64b31d63124e8fd41
Bergur Hallgrimsson <bergur@bergur.is>
2022-12-08 13:07:06 +0000

    Update updateCompetitor.js

--- repo/thor/updateCompetitor.js
+++ repo/thor/updateCompetitor.js
@@ -1,20 +1,22 @@
 const { VarChar } = require('mssql')
 
 function makeUpdateCompetitor (sqlPoolConnection) {
-  return function updateCompetitor (id, club) {
+  return function updateCompetitor (id, clubName, fullName) {
     const sql = `
       UPDATE
         [Athletics].[dbo].[Athl$Competitors]
       SET
-        [Félag] = @club
+        [Félag] = @clubName,
+        [Nafn] = @fullName
       WHERE
         [Númer] = @id`
 
     return sqlPoolConnection.then(pool => {
       const request = pool.request()
 
-      request.input('id', VarChar(20), id + '')
-      request.input('club', VarChar(10), club + '')
+      request.input('id', VarChar(20), id + '')      
+      request.input('clubName', VarChar(10), clubName + '')
+      request.input('fullName', VarChar(35), fullName + '')
 
       return request
         .query(sql)
