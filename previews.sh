5229eac9bdd9b6585ef5c37f24899c0a0341d5f9
bergur <bergur@bergur.is>
2022-12-16 01:53:13 +0000

    logging

---
+++ previews.sh
@@ -0,0 +1,6 @@
+curl --request POST \
+     --url "https://api.render.com/v1/services/srv-cedsdaun6mpkfr9aufh0/custom-domains" \
+     --header "accept: application/json" \
+     --header "Authorization: Bearer rnd_R4iIF7Ez5JJXZbqfyJ7LG60w9EHd" \
+     --header "content-type: application/json" \
+     --data-raw '{ "name": "freyr.previews.fri.is" }'
