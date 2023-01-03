e709e99e2084e6861c84e1c8502fc350db953d3d
bergur <bergur@bergur.is>
2023-01-03 12:55:48 +0000

    t

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -2,11 +2,12 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command, mode }) => {
-  if (command === 'build' && mode === 'test') {
-    console.log('building test')
-    console.log(process.env.RENDER_GIT_BRANCH)
-    console.log(process.env.VITE_FRI_API_URL)
+export default defineConfig(() => {
+  // render specific
+  if (process.env.IS_PULL_REQUEST === 'true') {
+    process.env.VITE_FRI_API_URL='https://'+ process.env.RENDER_GIT_BRANCH + '.' + process.env.FRI_API_HOST
+  } else {
+    process.env.VITE_FRI_API_URL='https://'+ process.env.FRI_API_HOST
   }
 
   return {
@@ -14,9 +15,5 @@ export default defineConfig(({ command, mode }) => {
     server: {
       port: 3001
     },
-    define: {
-      VITE_TEST: process.env.RENDER_GIT_BRANCH,
-      TEST: process.env.RENDER_GIT_BRANCH
-    }
   }
 })
