61879e32c8574282e345a875f626c4af62d2c51d
bergur <bergur@bergur.is>
2023-01-03 10:43:19 +0000

    fix bash

--- admin/vite-url.sh
+++ admin/vite-url.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo "vite-url"
-if [$IS_PULL_REQUEST == 'true']
+echo "setting vite env variable for api"
+if [ "$IS_PULL_REQUEST" == 'true' ]
 then
   export VITE_FRI_API_URL="https://$RENDER_GIT_BRANCH.$FRI_API_HOST"
 else 
