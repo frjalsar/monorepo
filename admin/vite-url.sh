ab99d3a879b18d89d5859b44c1513ec7ecd1c55f
bergur <bergur@bergur.is>
2023-01-03 10:25:05 +0000

    checking url

--- admin/vite-url.sh
+++ admin/vite-url.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+echo "vite-url"
 if [$IS_PULL_REQUEST == 'true'] then
+  echo "pull request"
   export VITE_FRI_API_URL="https://$RENDER_GIT_BRANCH.$FRI_API_HOST"
 else 
   export VITE_FRI_API_URL="https://$FRI_API_HOST"
