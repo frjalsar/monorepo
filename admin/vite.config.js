cc1b30df68e8e428dbc1eaefe0befcf51779b3dd
bergur <bergur@bergur.is>
2023-01-03 12:57:13 +0000

    t

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -5,15 +5,15 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig(() => {
   // render specific
   if (process.env.IS_PULL_REQUEST === 'true') {
-    process.env.VITE_FRI_API_URL='https://'+ process.env.RENDER_GIT_BRANCH + '.' + process.env.FRI_API_HOST
+    process.env.VITE_FRI_API_URL = 'https://' + process.env.RENDER_GIT_BRANCH + '.' + process.env.FRI_API_HOST
   } else {
-    process.env.VITE_FRI_API_URL='https://'+ process.env.FRI_API_HOST
+    process.env.VITE_FRI_API_URL = 'https://' + process.env.FRI_API_HOST
   }
 
   return {
     plugins: [vue()],
     server: {
       port: 3001
-    },
+    }
   }
 })
