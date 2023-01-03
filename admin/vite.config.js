08b9d6a24cceaa143b9bf16d457b609be846266e
bergur <bergur@bergur.is>
2023-01-03 12:23:06 +0000

    config

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -2,9 +2,17 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  server: {
-    port: 3001
+export default defineConfig(({ command, mode }) => {
+  if (command === 'build' && mode === 'test') {
+    console.log('building test')
+    console.log(process.env.$RENDER_GIT_BRANCH)
+    console.log(process.env.VITE_FRI_API_URL)
+  }
+
+  return {
+    plugins: [vue()],
+    server: {
+      port: 3001
+    }
   }
 })
