1d3ce6248cd8b1b350d5d2071806afeb46dd5054
bergur <bergur@bergur.is>
2023-01-03 13:13:44 +0000

    test

--- admin/src/App.vue
+++ admin/src/App.vue
@@ -170,8 +170,8 @@ export default {
     }
   },
   mounted () {
-    console.log('import meta vite test', import.meta.env.VITE_TEST)
-    console.log('import meta test', import.meta.env.TEST)
+    console.log(FRI_API_URL)
+    console.log(COUNTRIES_API_URL)
     this.authorize().then(user => {
       if (!user) {
         this.$router.push({ path: '/' })
--- admin/vite.config.js
+++ admin/vite.config.js
@@ -2,12 +2,11 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig(() => {
-  // render specific
-  if (process.env.IS_PULL_REQUEST === 'true') {
-    process.env.VITE_FRI_API_URL = 'https://' + process.env.RENDER_GIT_BRANCH + '.' + process.env.FRI_API_HOST
-  } else {
-    process.env.VITE_FRI_API_URL = 'https://' + process.env.FRI_API_HOST
+export default defineConfig(({ command, mode }) => {  
+  if (command === 'build' && mode === 'test' && process.env.IS_PULL_REQUEST === 'true') {
+    const pos = process.env.VITE_FRI_API_URL.indexOf('api')
+    
+    process.env.VITE_FRI_API_URL = process.env.VITE_FRI_API_URL.substring(0, pos) + process.env.RENDER_GIT_BRANCH + '.' + process.env.VITE_FRI_API_URL.substring(pos);
   }
 
   return {
