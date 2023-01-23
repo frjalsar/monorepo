7bec73a994224f3b884b5504c9b85532a3a40e1b
bergur <bergur@bergur.is>
2023-01-23 21:48:01 +0000

    add auth logging

--- api-service/lib/authenticationHandler.js
+++ api-service/lib/authenticationHandler.js
@@ -1,17 +1,21 @@
 function makeAuthenticationHandler (getToken, getUser, setUser, selectUser, logger) {
   return function authenticate () {
     return (req, res, next) => {
+      logger.info('authentication handler')
       const token = getToken(req.headers)
       if (token) {
         return getUser(token)
           .then(reply => {
+            logger.info('got user', reply)
             if (!reply) {
               return selectUser({ token }).then(users => {
+                logger.info('select user from db')
                 if (users.length === 1) {
                   req.user = users[0]
                   setUser(users[0])
                   return next()
                 } else {
+                  logger.info('found no user in store nor db')
                   return res.sendStatus(401)
                 }
               })
