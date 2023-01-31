62485978a2e02bc39c1c4d4c0419e4682e902107
shanbel-kassa <shanbelkassa1887@gmail.com>
2023-01-31 09:54:47 +0300

    add typescript config

---
+++ types/index.d.ts
@@ -0,0 +1,13 @@
+interface Athlete {
+    id: string;
+    fullName: string
+    kt: string
+    birthyear: number
+    gender: string
+    country: string
+    thorId: string
+    lastCompeted: Date
+    club: any,
+    region: any,
+    membership: any[]
+  }
