702f0dc5752a56d4162464aa5f51a874a364cf3d
shanbel-kassa <shanbelkassa1887@gmail.com>
2023-02-06 09:55:06 +0300

    lib add userstore changed to typescript

---
+++ api-service/src/lib/getToken.ts
@@ -0,0 +1,18 @@
+import {parseCookie} from 'cookie'
+
+export function getToken (headers) {
+  let token
+  if (headers.cookie) {
+    token = parseCookie(headers.cookie).FRI_API
+  }
+
+  if (headers.authorization) {
+    const parts = headers.authorization.split(' ')
+    if (parts.length === 2 && parts[0] === 'Bearer') {
+      token = parts[1]
+    }
+  }
+
+  return token
+}
+
