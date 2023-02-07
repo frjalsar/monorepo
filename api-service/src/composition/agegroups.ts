2ae1215b2a81d4fbf44e662d11728072e35e3063
shanbel-kassa <shanbelkassa1887@gmail.com>
2023-02-07 12:05:58 +0300

    added type

--- api-service/src/composition/agegroups.ts
+++ api-service/src/composition/agegroups.ts
@@ -1,10 +1,11 @@
+import { SelectAgeGroups } from 'types/agegroup'
 import {makeSelectAgeGroups} from '../repo/agegroups/select'
 import {makeAgeGroupsRoute} from '../routes/agegroups'
 
 
 export function makeAgeGroups () {
 
-  const selectAgeGroups = makeSelectAgeGroups()
+  const selectAgeGroups:SelectAgeGroups = makeSelectAgeGroups()
 
   return makeAgeGroupsRoute(selectAgeGroups)
 }
--- api-service/src/composition/events.ts
+++ api-service/src/composition/events.ts
@@ -4,14 +4,15 @@ import {makeSelectEvents} from '../repo/events/select'
 import {makeEditEvent} from '../repo/events/edit'
 import {makeCreateEvent} from '../repo/events/create'
 import {makeEventsRouter} from '../routes/events'
+import { CreateEvent, EditEvent, SelectEvents } from 'types/events'
 
 export type MakeEventTypes = (db: PoolClient) => IRouter
 
 export function makeEvents (db) {
 
-  const selectEvents = makeSelectEvents(db)
-  const editEvent = makeEditEvent(db)
-  const createEvent = makeCreateEvent(db)
+  const selectEvents:SelectEvents = makeSelectEvents(db)
+  const editEvent:EditEvent = makeEditEvent(db)
+  const createEvent:CreateEvent = makeCreateEvent(db)
 
   return makeEventsRouter(selectEvents, editEvent, createEvent)
 }
